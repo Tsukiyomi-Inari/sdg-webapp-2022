cbd74a7f4205435c491e10853f201a17324c2cb1
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-07-22 19:07:30 -0400

    controller add and error handler

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,31 +1,15 @@
 //  Routing for Goals API handling HTTP requests to (GET) fetch, (PUT) update/replace, display data(POST), remove (DELETE)
 
 const express = require('express');
-const router = express.router();
-const {} = require('../controllers/goalController')
+const router = express.Router();
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+} = require('../controllers/goalController')
 
-
-router.get('/', (req,res)=> {
-    res.status(200).json({message : 'Get goals'})
-})
-
-router.post('/', (req,res)=> {
-    res.status(200).json({message : 'Set goal'})
-})
-
-router.put('/:id', (req,res)=> {
-    res.status(200).json({message : `Update goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req,res)=> {
-    res.status(200).json({message : `Delete goal ${req.params.id}`})
-})
-
-
-
-//const { protect } = require('../middleware/authMiddleware')
-
-// router.route('/').get(protect, getGoals).post(protect, setGoal)
-// router.route('/:id').delete(protect. deleteGoal).put(protect, updateGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
