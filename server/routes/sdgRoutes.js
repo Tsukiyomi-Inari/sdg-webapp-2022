ae9cdeaa14a1e5beff693c8b378e7007c50f5d0d
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-07-24 17:06:37 -0400

    inital REST API (goals)

--- server/routes/sdgRoutes.js
+++
@@ -1,9 +0,0 @@
-//  Routing for SDG API handling HTTP requests to (GET) fetch, (PUT) update/replace, display data(POST)
-
-/* const expess = require('express')
-const router = express.Router()
-const {
-    getSDG,
-    setSDG,
-    updateSDG,
-} = require('../controller/sdgController') */
