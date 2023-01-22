2258f9055c98cae19d03145f7f53604217c3d412
Katherine Bellman <katherine.bellman@dcmail.ca>
2023-01-22 14:56:20 -0500

    Style adjustment

--- client/src/components/SDGGrid.jsx
+++ client/src/components/SDGGrid.jsx
@@ -10,28 +10,28 @@ const SDGGrid = ()=> {
     return (
      <div>
         <div className="sdg_row1" >
-            <img src={SDG1} alt="SDG 1" height="100" style={style}/>
-            <img src={SDG2} alt="SDG 2" height="100" style={style}/>
-            <img src={SDG3} alt="SDG 3" height="100" style={style}/>
-            <img src={SDG4} alt="SDG 4" height="100" style={style}/>
-            <img src={SDG5} alt="SDG 5" height="100" style={style}/>
-            <img src={SDG6} alt="SDG 6" height="100" style={style}/>
+            <img src={SDG1} alt="SDG 1" height="150" style={style}/>
+            <img src={SDG2} alt="SDG 2" height="150" style={style}/>
+            <img src={SDG3} alt="SDG 3" height="150" style={style}/>
+            <img src={SDG4} alt="SDG 4" height="150" style={style}/>
+            <img src={SDG5} alt="SDG 5" height="150" style={style}/>
+            <img src={SDG6} alt="SDG 6" height="150" style={style}/>
         </div>
         <div className="sdg_row2">
-            <img src={SDG7} alt="SDG 7" height="100" style={style}/>
-            <img src={SDG8} alt="SDG 8" height="100" style={style}/>
-            <img src={SDG9} alt="SDG 9" height="100" style={style}/>
-            <img src={SDG10} alt="SDG 10" height="100" style={style}/>
-            <img src={SDG11} alt="SDG 11" height="100" style={style}/>
-            <img src={SDG12} alt="SDG 12" height="100" style={style}/>
+            <img src={SDG7} alt="SDG 7" height="150" style={style}/>
+            <img src={SDG8} alt="SDG 8" height="150" style={style}/>
+            <img src={SDG9} alt="SDG 9" height="150" style={style}/>
+            <img src={SDG10} alt="SDG 10" height="150" style={style}/>
+            <img src={SDG11} alt="SDG 11" height="150" style={style}/>
+            <img src={SDG12} alt="SDG 12" height="150" style={style}/>
         </div>
         <div className="sdg_row3" >
-            <img src={SDG13} alt="SDG 13" height="100" style={style}/>  
-            <img src={SDG14} alt="SDG 14" height="100" style={style}/>
-            <img src={SDG15} alt="SDG 15" height="100" style={style}/>
-            <img src={SDG16} alt="SDG 16" height="100" style={style}/>
-            <img src={SDG17} alt="SDG 17" height="100" style={style}/>
-            <img src={SDGlogo} alt="SDG logo" height="100" style={style}/>
+            <img src={SDG13} alt="SDG 13" height="150" style={style}/>  
+            <img src={SDG14} alt="SDG 14" height="150" style={style}/>
+            <img src={SDG15} alt="SDG 15" height="150" style={style}/>
+            <img src={SDG16} alt="SDG 16" height="150" style={style}/>
+            <img src={SDG17} alt="SDG 17" height="150" style={style}/>
+            <img src={SDGlogo} alt="SDG logo" height="150" style={style}/>
        
             
         </div>
